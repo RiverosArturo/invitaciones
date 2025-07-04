2cc032095d66855dca8e2bd13e4bb6fb855eedd2
Arturo Riveros <riveros45rturo@gmail.com>
2025-07-04 01:59:05 -0600

    ultimo cambio

--- src/components/ui/invitations/xv/ItinerarySection.tsx
+++ src/components/ui/invitations/xv/ItinerarySection.tsx
@@ -6,7 +6,7 @@ import { Dancing_Script, Lora } from 'next/font/google';
 import { gsap } from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { SparklesF } from '@/components';
+// import { SparklesF } from '@/components';
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
